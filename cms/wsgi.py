615eeef7e0215df397c33f291c21cf9b94026abf
jairocp <andresbbx@gmail.com>
2017-03-14 15:16:07 -0500

    cambios despliegue

--- cms/wsgi.py
+++ cms/wsgi.py
@@ -11,8 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from mezzanine.utils.conf import real_project_name
+from dj_static  import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
                       "%s.settings" % real_project_name("cms"))
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
